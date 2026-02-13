602c83582f4c303ea99c686db611b74400c8857a
zRenard <zrenard@gmail.com>
2026-02-13 10:02:57 +0100

    fix. Remove release notes from local check

--- local.sh
+++ local.sh
@@ -5,15 +5,12 @@ echo "Validating source files..."
 echo "-----------------------------------"
 echo "Validating JavaScript"
 npx eslint index.js
-npx eslint release_notes.js
 echo "-----------------------------------"
 echo "Validating HTML"
 npx htmlhint index.html
-npx htmlhint release_notes.html
 echo "-----------------------------------"
 echo "Validating CSS"
 npx stylelint index.css
-npx stylelint release_notes.css
 echo "-----------------------------------"
 echo "All source files validated successfully."
 echo "==================================="
@@ -23,15 +20,12 @@ mkdir -p out
 echo "-----------------------------------"
 echo "Minifying HTML"
 npx html-minifier --collapse-whitespace --remove-comments --minify-css true --minify-js true -o ./out/index.html index.html
-npx html-minifier --collapse-whitespace --remove-comments --minify-css true --minify-js true -o ./out/release_notes.html release_notes.html 
 echo "-----------------------------------"
 echo "Minifying JavaScript"
 npx terser index.js -o ./out/index.js --compress --mangle --comments "/eslint/"
-npx terser release_notes.js -o ./out/release_notes.js --compress --mangle --comments "/eslint/"
 echo "-----------------------------------"
 echo "Minifying CSS"
 npx csso-cli index.css --output ./out/index.css
-npx csso-cli release_notes.css --output ./out/release_notes.css
 echo "-----------------------------------"
 echo "Minification complete."
 echo "==================================="
@@ -39,18 +33,12 @@ echo "Starting validation of minified files..."
 echo "-----------------------------------"
 echo "Validating minified JavaScript"
 npx eslint --no-ignore out/index.js
-npx eslint --no-ignore out/release_notes.js
 echo "-----------------------------------"
 echo "Validating minified HTML"
 npx htmlhint out/index.html
-npx htmlhint out/release_notes.html
 echo "-----------------------------------"
 echo "Validating minified CSS"
 npx stylelint out/index.css
-npx stylelint out/release_notes.css
-echo "-----------------------------------"
-echo "Building release notes JSON..."
-python release_notes.py --num_commits 15 --output out/release_notes.json 
 echo "-----------------------------------"
 echo "All done!"
 echo "==================================="
